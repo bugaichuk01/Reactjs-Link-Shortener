bd1bf36990acb9d8ddd0cbbd5448904711454f84
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-04 11:51:02 +0300

    Refactor with AuthLayout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../../_hooks/useTypedSelector";
 import {useActions} from "../../_hooks/useActions";
 import {useNavigate} from "react-router";
 import {loginLocal} from "../../store/reducers/auth.slice";
-import {login} from "../../store/actionCreators/auth";
 import {Link} from "react-router-dom";
-import stringIsValid from "../../helpers/stringIsValid";
+import {AuthLayout} from "../AuthLayout/AuthLayout";
+import {login} from "../../store/actionCreators/auth";
 
 export const Login = () => {
-
-    const [loginValue, setLoginValue] = useState('');
-    const [password, setPassword] = useState('');
-    const [typoError, setTypoError] = useState('');
     const {loginStatus, loginError, accessToken} = useTypedSelector(state => state.auth);
     const dispatch = useActions();
     const navigate = useNavigate();
-
-    function changeLogin(e: React.ChangeEvent<HTMLInputElement>) {
-        setLoginValue(e.target.value)
-    }
-
-    function changePassword(e: React.ChangeEvent<HTMLInputElement>) {
-        setPassword(e.target.value)
-    }
 
     useEffect(() => {
         if (loginStatus === 200) {
@@ -31,41 +19,19 @@ export const Login = () => {
         }
     }, [dispatch, loginStatus, navigate, accessToken])
 
-    function sendLoginRequest() {
-        if (stringIsValid(loginValue, password)) {
-            setTypoError('Логин и пароль должны содержать символы')
-            setTimeout(() => {
-                setTypoError('')
-            }, 3000)
-            setLoginValue('')
-            setPassword('')
-            return
-        }
-        dispatch(login({username: loginValue, password: password}))
-    }
 
     return (
         <div className='container d-flex flex-column align-items-center text-center mt-5'>
             <h1>Вход</h1>
-            <input
-                className='form-control w-25 m-2'
-                type='text'
-                value={loginValue}
-                placeholder='Введите логин'
-                onChange={changeLogin}
-            />
-            <input
-                className='form-control w-25 m-2'
-                type='password'
-                value={password}
-                placeholder='Введите пароль'
-                onChange={changePassword}
-            />
-            <button className='btn btn-primary w-25 m-4' onClick={sendLoginRequest}>Войти</button>
-            {(loginError || typoError) && <div className="alert alert-danger w-25">{loginError || typoError}</div>}
-            <Link className='link' to={'/registration'}>
-                Не зарегистрированы?
-            </Link>
+            <AuthLayout
+                error={loginError}
+                textBtn={'Войти'}
+                request={(username: string, password: string) => dispatch(login({username, password}))}
+            >
+                <Link className='link' to={'/registration'}>
+                    Не зарегистрированы?
+                </Link>
+            </AuthLayout>
         </div>
     )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,63 +1,29 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {useTypedSelector} from "../../_hooks/useTypedSelector";
-import {useActions} from "../../_hooks/useActions";
-import stringIsValid from "../../helpers/stringIsValid";
-import {register} from "../../store/actionCreators/auth";
 import {Link} from "react-router-dom";
 import {Success} from "../../components/Success/Success";
+import {AuthLayout} from "../AuthLayout/AuthLayout";
+import {useActions} from "../../_hooks/useActions";
+import {register} from "../../store/actionCreators/auth";
+
 
 export const Register = () => {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [typoError, setTypoError] = useState('');
-    const error = useTypedSelector(state => state.auth.registerError);
-    const registerStatus = useTypedSelector(state => state.auth.registerStatus);
+    const {registerStatus, registerError} = useTypedSelector(state => state.auth);
     const dispatch = useActions();
-
-    function changeLogin(e: React.ChangeEvent<HTMLInputElement>) {
-        setLogin(e.target.value)
-    }
-
-    function changePassword(e: React.ChangeEvent<HTMLInputElement>) {
-        setPassword(e.target.value)
-    }
-
-    function sendRegRequest() {
-        if (stringIsValid(login, password)) {
-            setTypoError('Логин и пароль должны содержать символы')
-            setTimeout(() => {
-                setTypoError('')
-            }, 3000)
-            setLogin('')
-            setPassword('')
-            return
-        }
-        dispatch(register({username: login, password: password}))
-    }
 
     return (
         <div className='container d-flex flex-column align-items-center text-center mt-5'>
             <h1>Регистрация</h1>
-            <input
-                className='form-control w-25 m-2'
-                type='text'
-                value={login}
-                placeholder='Ваш логин'
-                onChange={changeLogin}
-            />
-            <input
-                className='form-control w-25 m-2'
-                type='password'
-                value={password}
-                placeholder='Придумайте пароль'
-                onChange={changePassword}
-            />
-            <button className='btn btn-primary w-25 m-4' onClick={sendRegRequest}>Зарегистрироваться</button>
-            {(error || typoError) && <div className="alert alert-danger w-25">{error || typoError}</div>}
-            {registerStatus === 200 && <Success/>}
-            <Link className='link' to={'/login'}>
-                Уже имеете аккаунт?
-            </Link>
+            <AuthLayout
+                error={registerError}
+                textBtn={'Зарегистрироваться'}
+                request={(username: string, password: string) => dispatch(register({username, password}))}
+            >
+                {registerStatus === 200 && <Success/>}
+                <Link className='link' to={'/login'}>
+                    Уже имеете аккаунт?
+                </Link>
+            </AuthLayout>
         </div>
     )
 }
